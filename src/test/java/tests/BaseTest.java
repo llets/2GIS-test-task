35e161cb8658b5aa47dfd07e25d13e3e45919218
Alexa <rizar_sasha@mail.ru>
2025-09-06 18:10:10 +0500

    Changes: 1. Добавление метода setup для корректного отображения кириллицы в консоли

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,20 +1,35 @@
 package tests;
 
+import io.restassured.RestAssured;
+import io.restassured.config.DecoderConfig;
+import io.restassured.config.RestAssuredConfig;
 import io.restassured.http.Cookie;
 import io.restassured.response.Response;
 import models.Place;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import requests.v1.auth.tokens.TokenRequest;
 import utils.ConfigAndDataUtils;
 
 import java.util.List;
 
+import static io.restassured.config.EncoderConfig.*;
+
 public class BaseTest {
 
     protected Cookie cookie = null;
 
+    @BeforeClass
+    public static void setup() {
+        RestAssured.config = RestAssuredConfig.config()
+                .encoderConfig(encoderConfig()
+                        .defaultContentCharset("UTF-8"))
+                .decoderConfig(DecoderConfig.decoderConfig()
+                        .defaultContentCharset("UTF-8"));
+    }
+
     @BeforeMethod
-    public void getToken(){
+    public void getToken() {
         Response response = TokenRequest.performPost();
         System.out.println(response.getCookie("token"));
         this.cookie = new Cookie.Builder("token", response.getCookie("token")).build();
